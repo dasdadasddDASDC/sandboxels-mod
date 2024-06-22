3a28efc81d43f5011fccdce2153ef5c5e1bf51bd
dasdadasddDASDC <127432980+dasdadasddDASDC@users.noreply.github.com>
2024-06-22 18:46:01 +0200

    Update lipton.js

--- lipton.js
+++ lipton.js
@@ -1,4 +1,14 @@
-// Define the Lipton drink element
+// This is how to add a new mod to the game.
+
+// Create a new Javascript file like this one.
+// Add the file to the mods folder on GitHub, or host it somewhere else.
+// https://github.com/R74nCom/sandboxels/tree/main/mods
+
+// To add it in the Mod Loader:
+// If it is in the mods folder, you can just use the name of the file.
+// If it is hosted somewhere else, you can use the full URL, including the HTTPS://.
+
+// Adding elements:
 elements.lipton = {
     color: "#ffa500", // Orange color (you can adjust as needed)
     behavior: behaviors.LIQUID,
@@ -7,3 +17,65 @@ elements.lipton = {
     state: "liquid",
     density: 1020, // Adjust density as needed
 };
+
+// Changing existing elements (example provided for water):
+elements.water.color = "#ff0000";
+elements.water.behavior = behaviors.WALL;
+
+// Removing elements:
+// Be aware, things may break
+delete elements.ketchup;
+
+// Adding behavior presets:
+behaviors.SELFDELETE = [
+    "XX|XX|XX",
+    "XX|DL|XX",
+    "XX|XX|XX",
+];
+
+// Raw JavaScript behaviors:
+behaviors.mud.tick = function(pixel) {
+    if (tryMove(pixel, pixel.x, pixel.y+1)) {
+        console.log("Moved!");
+    }
+    else {
+        console.log("Couldn't move!")
+    }
+};
+
+// Create a new tool:
+elements.sand_exploder = {
+    color: "#ff0000",
+    tool: function(pixel) {
+        if (pixel.element == "sand") {
+            pixel.element = "explosion"
+        }
+    },
+    category: "tools",
+};
+
+// Add reactions to existing elements
+if (!elements.water.reactions) { // Include this block once
+    elements.water.reactions = {} // This creates the property if it doesn't exist
+}
+elements.water.reactions.lipton = { "elem1":null, "elem2":"lipton_water" }
+elements.water.reactions.soap = { "elem1":null, "elem2":"soapy_water" }
+
+// Run after all mods are loaded, for cross-mod compatibility
+runAfterLoad(function() {
+    // Your code here
+    console.log("Lipton mod loaded!");
+});
+
+// Run if another mod is active
+if (enabledMods.includes("mods/test.js")) {
+    runAfterLoad(function() {
+        // Your code here
+        console.log("Lipton mod loaded with test mod!");
+    });
+}
+
+// Creating eLists:
+eLists.CONDIMENT = ["ketchup","melted_cheese","mayo"];
+// Adding elements to eLists:
+eLists.CONDIMENT.push("lipton");
